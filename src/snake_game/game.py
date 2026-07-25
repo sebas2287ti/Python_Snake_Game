b791c7f17acf6aaae364f88d721b6d662999aae4
sebas2287ti <sebas2287ti@gmail.com>
2026-07-25 15:56:33 -0500

    add: Movent snake

--- src/snake_game/game.py
+++ src/snake_game/game.py
@@ -2,11 +2,17 @@ import snake_game.config as Config_Map
 import snake_game.utils as Utils_Game
 
 fruits_eats = 0
+snake_data = (1,1, "d")
 
 def fruits_eat():
     global fruits_eats
     fruts_eats =+ 1
 
+def modify_snake(x,y, direction):
+    global snake_data
+    snake_data = (x,y, direction)
+
 map_game = Utils_Game.create_map()
 def Start_Game():
     pass
+
--- src/snake_game/snake.py
+++ src/snake_game/snake.py
@@ -1,6 +1,6 @@
 from snake_game.utils import dict_direction as direction_snake
 from snake_game.config import map_size
-from snake_game.game import map_game, fruits_eats
+from snake_game.game import map_game, fruits_eats, snake_data
 
 def check_run_snake_collision(y_snake, x_snake, movent):
     y_movent, x_movent = direction_snake[movent]
@@ -17,3 +17,31 @@ def check_run_snake_collision(y_snake, x_snake, movent):
             return True
     else:
         return True
+
+def movent_snake():
+    global snake_data
+    y, x, direction = snake_data 
+    movent_y, movent_x = direction_snake[direction]
+
+    try:
+        tecla = obtener_tecla().lower()
+        if tecla in direction_snake:
+            pass
+        else:
+            raise ValueError("Movimiento no valido")
+    except: 
+        new_y, new_x = y + movent_y, x + movent_x
+        snake_data = (new_y,new_x,direction)
+    else:
+        movent_y, movent_x = direction_snake[tecla]
+        new_y, new_x = y + movent_y, x + movent_x
+        snake_data = (new_y, new_x, tecla)
+    print(snake_data)
+    return snake_data 
+        
+
+def obtener_tecla():
+    return input("movimiento: ")
+
+
+movent_snake()
